c829f394b0130150f639afdffa8fc6376b19c318
Josua Manullang <j7.manullang@gmail.com>
2021-10-23 14:23:35 +0700

    [+] create root file

---
+++ index.js
@@ -0,0 +1,8 @@
+const root = document.getElementById('root');
+const heading1 = document.createElement('h1'); 
+const heading1Title = '3rd';
+
+heading1.textContent = heading1Title;
+root.appendChild(heading1);
+
+console.log(root);
