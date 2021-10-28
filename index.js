2b6709015a64481dede9b554ff9a0f1bc15afb16
Josua Manullang <j7.manullang@gmail.com>
2021-10-28 22:34:56 +0700

    [^] membuat "content header" atau element HTML melalui javascript

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const root = document.getElementById('root');
-const heading1 = document.createElement('h1'); 
-const heading1Title = '3rd';
 
-heading1.textContent = heading1Title;
-root.appendChild(heading1);
+const header = () => {
+    root.insertAdjacentHTML(
+        'beforebegin',
+        `
+            <header class="head">
+                  <div class="head-logo">
+                        <img class="head-logo-image"
+                             src="favicon_io/android-chrome-192x192.png"
+                             alt="03.tiga">
+                        <h1 class="head-logo-label">dua</h1>
+                  </div>
+            </header>
+        `,
+    );
+};
 
-console.log(root);
+const initPage = () => {
+    header();
+};
+
+window.onload = initPage();
