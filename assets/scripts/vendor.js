5f43015c53a09f082a1b7ba5496c6345064ea1eb
Josua Manullang <j7.manullang@gmail.com>
2021-11-26 23:54:37 +0700

    [+] mulai memproses input data dan indicator nilai untuk => pengeluaran,anggaran

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,23 +1,28 @@
 const balanceBtn = document.getElementById('expense-balance__assign-button');
-
-const backdrop = document.getElementById('backdrop');
-const balances = document.querySelector('#balance');
-
-const cancelDialog = document.querySelectorAll('.assign-cancel');
-const payments = document.getElementById('payment');
-const bills = document.getElementById('bill');
-const savings = document.getElementById('saving');
-
 const paymentLink = document.getElementById('content__payments');
 const billLink = document.getElementById('content__bills');
 const savingLink = document.getElementById('content__savings');
 
-const balanceAmount = document.getElementById('balance-amount');
+const expensesBar = document.getElementById('expenses-minus');
+const balanceBar = document.getElementById('expenses-balance');
+
+const backdrop = document.getElementById('backdrop');
+const balances = document.getElementById('balance');
+const payments = document.getElementById('payment');
+const bills = document.getElementById('bill');
+const savings = document.getElementById('saving');
+
+const balanceAmount = document.getElementById('balance-amount__value');
 const savingAmount = document.getElementById('saving-amount');
 const paymentTitle = document.getElementById('payment-title');
 const paymentAmount = document.getElementById('payment-amount');
 const billTitle = document.getElementById('bill-title');
 const billAmount = document.getElementById('bill-amount');
+
+const cancelAssignBtn = document.querySelectorAll('.assign-cancel');
+const saveBalanceBtn = document.getElementById('save-balance');
+const saveBillBtn = document.getElementById('save-bill');
+const saveSavingBtn = document.getElementById('save-saving');
 
 function hideBackDrop() {
     backdrop.classList.remove('open');
@@ -62,11 +67,33 @@ function hideDialog() {
     backdrop.classList.remove('open');
 }
 
+const adjustExpenseBalanceBars = (maxAmount) => {
+    expensesBar.max = maxAmount;
+    expensesBar.value = 0;
+    balanceBar.max = maxAmount;
+    balanceBar.value = maxAmount;
+};
+
+const dealtExpense = (expenseAmount) => {
+    expensesBar.value += expenseAmount;
+    balanceBar.value -= expenseAmount;
+};
+
+const getUserBudget = () => {
+    const budgetVal = parseInt(balanceAmount.value);
+    isNaN(budgetVal)
+        ? alert('Must a number')
+        : adjustExpenseBalanceBars(budgetVal);
+};
+
 balanceBtn.addEventListener('click', showAddBalance);
 paymentLink.addEventListener('click', showPayments);
 billLink.addEventListener('click', showBills);
 savingLink.addEventListener('click', showSavings);
-cancelDialog.forEach(function (elem) {
+
+saveBalanceBtn.addEventListener('click', getUserBudget);
+
+cancelAssignBtn.forEach(function (elem) {
     elem.addEventListener('click', hideDialog);
 });
 backdrop.addEventListener('click', hideBackDrop);
